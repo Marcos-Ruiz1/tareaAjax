b6d346207bfa0b9e35533344df65bbfd1c5a74e6
Marcos-Ruiz1 <Marcos.ZazuetaRuiz1@gmail.com>
2024-05-05 18:04:01 -0700

    first commit

---
+++ src/main/java/com/mycompany/laboratoriowebajax_235765_233244_216354/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.laboratoriowebajax_235765_233244_216354;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
